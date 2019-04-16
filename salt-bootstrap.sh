5c6bc8da172c51132048f6eabaf27f22952c3834
Gareth J. Greenaway <gareth@wiked.org>
2019-04-16 13:42:53 -0700

    initial add.

---
+++ salt-bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+curl https://repo.saltstack.com/osx/salt-2018.3.2-py2-x86_64.pkg.md5 > /tmp/salt-2018.3.2-py2-x86_64.pkg.md5
+
+curl https://repo.saltstack.com/osx/salt-2018.3.2-py2-x86_64.pkg > /tmp/salt-2018.3.2-py2-x86_64.pkg
+
+set `cat /tmp/salt-2018.3.2-py2-x86_64.pkg.md5` ; echo $1 | perl -ne \"print lc\"
+
+md5 -q /tmp/salt-2018.3.2-py2-x86_64.pkg
+
+installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target / ; exit 0
+
+source /etc/profile ; pip install psutil
+
