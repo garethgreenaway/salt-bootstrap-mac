8c34d08b6df1de3251f8f8c9beb6154a51d215ad
Gareth J. Greenaway <gareth@wiked.org>
2019-04-16 13:56:58 -0700

    removing quotes, adding sudo.

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -4,11 +4,11 @@ curl https://repo.saltstack.com/osx/salt-2018.3.2-py2-x86_64.pkg.md5 > /tmp/salt
 
 curl https://repo.saltstack.com/osx/salt-2018.3.2-py2-x86_64.pkg > /tmp/salt-2018.3.2-py2-x86_64.pkg
 
-set `cat /tmp/salt-2018.3.2-py2-x86_64.pkg.md5` ; echo $1 | perl -ne \"print lc\"
+set `cat /tmp/salt-2018.3.2-py2-x86_64.pkg.md5` ; echo $1 | perl -ne "print lc"
 
 md5 -q /tmp/salt-2018.3.2-py2-x86_64.pkg
 
-installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target / ; exit 0
+sudo installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target / ; exit 0
 
 source /etc/profile ; pip install psutil
 
