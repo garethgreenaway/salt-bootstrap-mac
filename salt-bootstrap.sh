4266fbda61fd1dbbfbfb7c15fa7eee768b2b1cf1
Gareth J. Greenaway <gareth@wiked.org>
2019-04-17 10:25:38 -0700

    Adding some echos to see what is happening.  Changing to a source /etc/profile in .bashrc.

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
+echo "Downloading MD5"
 curl https://repo.saltstack.com/osx/salt-2018.3.2-py2-x86_64.pkg.md5 > /tmp/salt-2018.3.2-py2-x86_64.pkg.md5
 
+echo "Downloading Salt Package"
 curl https://repo.saltstack.com/osx/salt-2018.3.2-py2-x86_64.pkg > /tmp/salt-2018.3.2-py2-x86_64.pkg
 
 set `cat /tmp/salt-2018.3.2-py2-x86_64.pkg.md5` ; echo $1 | perl -ne "print lc"
 
 md5 -q /tmp/salt-2018.3.2-py2-x86_64.pkg
 
+echo "Installing Salt Package"
 sudo installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target / ; exit 0
 
+echo "Installing psutil"
 source /etc/profile ; pip install psutil
 
 sudo launchctl list
 
+echo "Unloading Salt Minion"
 sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
 
+echo "Loading Salt Minion"
 sudo launchctl load /Library/LaunchDaemons/com.saltstack.salt.minion.plist
 
-echo "export PATH=$PATH:/opt/salt/bin" >> /Users/jenkins/.bashrc
+echo "Creating /Users/jenkins/.bashrc"
+echo "source /etc/profile" >> /Users/jenkins/.bashrc
