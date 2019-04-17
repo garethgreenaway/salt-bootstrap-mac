cb64bf1badfdfeb2c870a7c16799ecb7f8be53cb
Gareth J. Greenaway <gareth@wiked.org>
2019-04-17 10:31:12 -0700

    removing exit 0.

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -11,7 +11,7 @@ set `cat /tmp/salt-2018.3.2-py2-x86_64.pkg.md5` ; echo $1 | perl -ne "print lc"
 md5 -q /tmp/salt-2018.3.2-py2-x86_64.pkg
 
 echo "Installing Salt Package"
-sudo installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target / ; exit 0
+sudo installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target /
 
 echo "Installing psutil"
 source /etc/profile ; pip install psutil
