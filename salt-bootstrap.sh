d9ab4620bb64b199e3c06ab1df7015c08639dcbf
Gareth J. Greenaway <gareth@wiked.org>
2019-04-17 09:21:05 -0700

    Ensure path for Salt executables is set for no login shells.

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -12,3 +12,10 @@ sudo installer -pkg /tmp/salt-2018.3.2-py2-x86_64.pkg -target / ; exit 0
 
 source /etc/profile ; pip install psutil
 
+sudo launchctl list
+
+sudo launchctl unload /Library/LaunchDaemons/com.saltstack.salt.minion.plist
+
+sudo launchctl load /Library/LaunchDaemons/com.saltstack.salt.minion.plist
+
+echo "export PATH=$PATH:/opt/salt/bin" >> ~/jenkins/.bashrc
